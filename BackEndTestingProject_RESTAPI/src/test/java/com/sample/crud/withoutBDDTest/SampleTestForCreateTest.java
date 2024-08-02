5c9f93a97930845e26b134e2d831c4f2abc3b5bd
GAGAN M S <gaganms15@gmail.com>
2024-08-02 18:28:16 +0530

    5

--- BackEndTestingProject_RESTAPI/src/test/java/com/sample/crud/withoutBDDTest/SampleTestForCreateTest.java
+++ BackEndTestingProject_RESTAPI/src/test/java/com/sample/crud/withoutBDDTest/SampleTestForCreateTest.java
@@ -12,7 +12,7 @@ public class SampleTestForCreateTest {
 	@Test
 	public void postDataToServer()
 	{
-		JSONObject jsonObj=new JSONObject();   //Here(In line#15 to 19)we are creating the payload(BODY) of the Request(After adding 'JSON simple' dependencies from MVNREPOSITORY).
+		JSONObject jsonObj=new JSONObject();   //Here(In line#15 to 19)we are creating the payload(BODY)of the Request(After adding 'JSON simple' dependencies from MVNREPOSITORY).
 		jsonObj.put("createdBy", "GAGAN");     //This JSONObject is exactly similar to HashMap(ie, we already knows, in HashMap will always store the data in KEY-VALUE pairs only).
 		jsonObj.put("projectName", "Orange1");
 		jsonObj.put("status", "Created");
@@ -20,7 +20,7 @@ public class SampleTestForCreateTest {
 
 		RequestSpecification req = RestAssured.given();	  //Eventhough all the CRUD operations(HTTP Methods) are available in 'RestAssured' class,but we can't use Post()(except get(),we can't use any other methods like,post(),patch(),delete()),bcz before sending post request I need to write Request body or I have to set some pre-conditions & Those precondition methods like, body(),contenttype() are available in 'RequestSpecification' Interface. 
 		req.contentType(ContentType.JSON);               //Here,line# 23 to 24 are the preconditions.
-		req.body(jsonObj.toJSONString());               //Here we are converting the 'jsonObj' into 'toJSONString' & this conversion should be mandatory bcz,which will convert this Map(ie,jsonObj)into complete Json format of string before passing inside the body(this has to be done bcz,we don't have/installed Json Parser tool to convert).
+		req.body(jsonObj.toJSONString());               //Here we are converting the 'jsonObj' into 'toJSONString' & this conversion should be mandatory bcz,which will convert this Map(ie,jsonObj)into complete 'Json format of string' before passing inside the body(this has to be done bcz,we don't have/installed Json Parser tool to convert).
 
 		
 		
